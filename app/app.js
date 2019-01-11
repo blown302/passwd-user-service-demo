1eade04b0160d4472aa6427ede574c7f0a5252b0
Thomas Milas <itpro41994@gmail.com>
2019-01-10 21:00:19 -0800

    added request logging

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 const {PORT} = require('./config/config');
 const userRouter = require('./routes/users.route');
@@ -8,6 +9,11 @@ const app = express();
 
 app.get('/ping', (req, res) => res.end());
 app.use(bodyParser.json());
+
+// include request logging
+app.use(morgan('combined'));
+
+// include routers
 app.use('/users', userRouter);
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
